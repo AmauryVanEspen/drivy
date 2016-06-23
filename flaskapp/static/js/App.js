a44da472618f647f870bfe8ff5114487e8b05962
afrancisboeuf <afrancisboeuf@gmail.com>
2016-06-23 16:08:06 -0700

    flask et react marchent ensemble

--- flaskapp/static/js/App.js
+++ flaskapp/static/js/App.js
@@ -1,5 +1,5 @@
 
-require("../css/style.less");
+require("../less/style.less");
 
 import React from 'react';
 import ReactDOM from 'react-dom';
--- flaskapp/views/main/views.py
+++ flaskapp/views/main/views.py
@@ -3,8 +3,7 @@ from flask.ext.login import current_user
 
 from . import main
 
-
 @main.route('/')
 def index():
-    return render_template('main/index.html')
+    return render_template('app.html')
 
